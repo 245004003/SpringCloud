7895accf7aaf0ce2db6f43ca653986b3fa31ada3
245004003 <245004003@qq.com>
2020-03-17 19:06:50 +0800

    hellow

---
+++ hellow.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args){
+	       System.out.println("Hello World");
+	       System.out.println("Hello BUG");
+	       System.out.println("Hello web");
+	       System.out.println("Hello java");
+	}
+}
