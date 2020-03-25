823bd6eeef2ee48e364654aa078dafab77ec1c3c
245004003 <245004003@qq.com>
2020-03-25 21:27:10 +0800

    commit

--- cloud-provider-payment8001/src/main/java/com/newer/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/newer/springcloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package com.newer.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @create 2020-03-08 17:16
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
